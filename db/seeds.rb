78ce3dbe840dcef856cf74849e8ec4e7032c068d
denis <d.kalesnikov@itfnc.com>
2016-03-18 14:20:18 +0700

    (+) project and employee_to_project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Employee.delete_all
-Position.delete_all
+Employee.destroy_all
+Position.destroy_all
+Project.destroy_all
+
+# Для удаления важно использовать destroy_all для того чтобы вызывались коллбэки
+# и выполнялось каскадное удаление
 
 positions = (0..4).to_a.map do
+
+  # Для тестовых данных используем библиотеку faker
+  # https://github.com/stympy/faker
+
   Position.create! name: Faker::Company.profession
 end
 
+projects = (0..4).to_a.map do
+  Project.create! name: Faker::Lorem.sentence
+end
+
 10.times do
-  Employee.create! full_name: Faker::Name.name,
+  employee = Employee.create! full_name: Faker::Name.name,
                    position: positions.sample,
                    job_start: rand(3..5).years.ago
+
+  rand(1..2).times do
+    employee.projects << projects.sample
+  end
 end
+
+
+
