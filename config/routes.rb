4c8d71fabff53cd2b32adf413ee897ef328be730
denis <d.kalesnikov@itfnc.com>
2016-04-05 19:00:15 +0700

    (+) home controller path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :employees
-  root 'employees#index'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
